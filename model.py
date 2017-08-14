63ed2ceef09d1ed857951d20ffea1de35030d923
Matteo Dell'Amico <Matteo_DellAmico@symantec.com>
2017-08-14 08:15:44 +0200

    Some docs on how logprobs are computed.

--- model.py
+++ model.py
@@ -16,15 +16,30 @@ import numpy as np
 
 
 class Model(metaclass=abc.ABCMeta):
+    """Abstract base class for models.
+
+    For each probability p, we handle its logprob -- i.e., with some
+    abuse of notation, the base-2 logarithm changed of sign:
+    -math.log2(p).
+    """
 
     @abc.abstractmethod
-    def generate(self): pass
+    def generate(self):
+        """Generate a random password according to the model.
+
+        Returns (logprob, passwd); passwd is the random password and
+        logprob is its probability.
+        """
+        pass
 
     def sample(self, n):
+        """Generate a sample of n passwords."""
         return (self.generate() for _ in range(n))
 
     @abc.abstractmethod
-    def logprob(self, word): pass
+    def logprob(self, word):
+        """Return the logprob of word according to the model."""
+        pass
 
 
 class PosEstimator:
